6dd2b604445f794c64bc4918298af6e0bf405294
Yogesh Bhatt <me@yogicodes.com>
2024-08-25 20:56:37 +0530

    feat: ClerkProvider instead of SessionProvider

--- components/layout/providers.tsx
+++ components/layout/providers.tsx
@@ -1,18 +1,40 @@
 'use client';
 import React from 'react';
 import ThemeProvider from './ThemeToggle/theme-provider';
-import { SessionProvider, SessionProviderProps } from 'next-auth/react';
+import { ClerkProvider } from '@clerk/nextjs';
+import { experimental_createTheme } from '@clerk/themes';
+
+const customClerkTheme = experimental_createTheme({
+  variables: {
+      colorBackground: '#09090b',
+      colorNeutral: 'white',
+      colorPrimary: '#ffffff',
+      colorTextOnPrimaryBackground: 'black',
+      colorText: 'white',
+      colorInputText: 'white',
+      colorInputBackground: '#09090b',
+  },
+  elements: {
+      providerIcon__apple: { filter: 'invert(1)' },
+      providerIcon__github: { filter: 'invert(1)' },
+      activeDeviceIcon: {
+          '--cl-chassis-bottom': '#d2d2d2',
+          '--cl-chassis-back': '#e6e6e6',
+          '--cl-chassis-screen': '#e6e6e6',
+          '--cl-screen': '#111111',
+      },
+  },
+})
+
 export default function Providers({
-  session,
   children
 }: {
-  session: SessionProviderProps['session'];
   children: React.ReactNode;
 }) {
   return (
     <>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <SessionProvider session={session}>{children}</SessionProvider>
+        <ClerkProvider appearance={{ baseTheme: customClerkTheme }}>{children}</ClerkProvider>
       </ThemeProvider>
     </>
   );
